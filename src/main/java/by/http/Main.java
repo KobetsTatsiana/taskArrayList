c990a9209c7f4eb828f0b42ef0717be9d3a45f95
Tatsiana Kobets <tatsiana.kobets@yandex.ru>
2023-09-14 03:27:52 +0300

    Final check

--- src/main/java/by/http/Main.java
+++ src/main/java/by/http/Main.java
@@ -1,21 +1,54 @@
 package by.http;
 
+/**
+ * @autor Tatsiana Kobets
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        MethodsOfWorkingWithArrayList methodsOfWorkingWithArrayList = new MethodsOfWorkingWithArrayList();
+        /**
+         * Filling the list with values and working with ArrayList.
+         */
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(3);
+        myArrayList.add(2);
+        myArrayList.add(3);
+        myArrayList.add(9);
+        System.out.println("Primary array: " + myArrayList);
 
-        methodsOfWorkingWithArrayList.createdArrayList();
-        methodsOfWorkingWithArrayList.addedElementToTheEndOfTheList();
-        methodsOfWorkingWithArrayList.addedElementAtIndexOne();
-        methodsOfWorkingWithArrayList.gettingElementAtIndexTwo();
-        methodsOfWorkingWithArrayList.replacingElementAtIndexOne();
-        methodsOfWorkingWithArrayList.replacingTheFirstElementWithTheLast();
-        methodsOfWorkingWithArrayList.removeElementAtIndexTwo();
-        methodsOfWorkingWithArrayList.quickListSorting();
-        methodsOfWorkingWithArrayList.sorting();
-        methodsOfWorkingWithArrayList.createdListAndSortComparableWithComparator();
-        methodsOfWorkingWithArrayList.deleteEntireCollection();
+        myArrayList.add(1000);
+        System.out.println("Replacement element (1000) to the end of the list: " + myArrayList);
+
+        myArrayList.set(1, 7);
+        System.out.println("Added element 7 at index 1: " + myArrayList);
+
+        System.out.println("Size list: " + myArrayList.size());
+
+        System.out.println("Getting element at index 2: " + myArrayList.get(2));
+
+        myArrayList.remove(2);
+        System.out.println("Remove element at index 2: " + myArrayList);
+
+        myArrayList.replacingTheFirstElementWithTheLast();
+        System.out.println("Replacing the first element with the last: " + myArrayList);
+
+        myArrayList.clear();
+        System.out.println("List after clearing: " + myArrayList);
+
+        System.out.println("Sorting");
+        SortMyArrayList<Integer> list = new SortMyArrayList<>();
+        list.add(10);
+        list.add(3);
+        list.add(7);
+
+        System.out.print("Original List: ");
+        list.display();
+        list.quickSort();
+        System.out.print("Sorted List: ");
+        list.display();
+        list.reverseSort();
+        System.out.print("Sorted reverse List: ");
+        list.display();
     }
 }
